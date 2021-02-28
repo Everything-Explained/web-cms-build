e14ffc55e469f0741240d8301c20db776a964b47
Jaeiya <exanix@gmail.com>
2021-02-28 13:45:24 -0800

    Add typings and summary support to literature

--- services/api_literature.ts
+++ services/api_literature.ts
@@ -1,11 +1,24 @@
-import { getStories, mapStoryToPage, Story, StoryVersion } from "./api_storyblok";
+import { getStories, mapStoryToPage, Story, StoryblokContent, StoryVersion } from "./api_storyblok";
 
+interface LiteratureContent extends StoryblokContent {
+  summary: string;
+}
+
+interface Literature extends Story {
+  content: LiteratureContent;
+}
 
 export async function getLiterature(slug: string, version: StoryVersion = 'published') {
-  const stories = await getStories<Story>({
+  const stories = await getStories<Literature>({
     starts_with: slug,
     sort_by: 'created_at:asc',
     version,
   });
-  return stories.map(mapStoryToPage);
+  return stories.map(story => {
+    const page = mapStoryToPage(story);
+    return {
+      ...page,
+      summary: story.content.summary
+    };
+  });
 }
