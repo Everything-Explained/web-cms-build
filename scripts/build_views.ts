bd243fe8a8a7cd73f3e125d2b03d3e97bc949817
Jaeiya <exanix@gmail.com>
2021-03-15 14:25:46 -0700

    Add changelog build support

--- scripts/build_views.ts
+++ scripts/build_views.ts
@@ -4,22 +4,33 @@ import { getPages } from '../services/api_pages';
 import { getVideos } from '../services/api_videos';
 import paths from '../paths';
 import { getLiterature } from '../services/api_literature';
+import { getChangelogs } from '../services/api_changelog';
 
 
 
 export async function bundleMDPages() {
   const posts = await getBlogPosts();
   const pages = await getPages();
+  const changelogs = await getChangelogs();
   const videos = await getVideos('red33m/videos');
-  const r3d_lit = await getLiterature('red33m/literature');
+  const r3d_lit = await getLiterature('red33m/literature', 'draft');
   const lib_lit = await getLiterature('library/literature');
 
   await bundler.bundlePageMaps([
     { dir: `${paths.dist.pages}/blog.json`,         pages: posts        },
     { dir: `${paths.dist.pages}/home.json`,         pages: [pages.home] },
+    { dir: `${paths.dist.pages}/changelog.json`,    pages: changelogs,  },
     { dir: `${paths.dist.library}/literature.json`, pages: lib_lit      },
     { dir: `${paths.dist.red33m}/videos.json`,      pages: videos       },
     { dir: `${paths.dist.red33m}/literature.json`,  pages: r3d_lit      }
+  ], 'html');
+}
+
+
+export async function buildChangelog() {
+  const changelogs = await getChangelogs();
+  await bundler.bundlePageMaps([
+    { dir: `${paths.dist.pages}/changelog.json`,    pages: changelogs,  },
   ], 'html');
 }
 
