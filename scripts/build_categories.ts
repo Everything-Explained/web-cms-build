20702a529956f8ff4b587e5026c3b15dc93c5281
Jaeiya <exanix@gmail.com>
2021-04-28 18:31:48 -0700

    Fix category video map to reflect frontend needs
    Overhauls the category video mapping to create a
    video map that does not need parsing on the front
    end. Categories are now objects within an array.
    
    Code has also been cleaned.

--- scripts/build_categories.ts
+++ scripts/build_categories.ts
@@ -1,10 +1,17 @@
 import { Page } from '@everything_explained/web-md-bundler/dist/core/md_page_bundler';
 import { mkdirSync, existsSync } from 'fs';
 import { writeFile } from 'fs/promises';
-import { getVideos } from '../services/api_videos';
+import { getVideos, Video } from '../services/api_videos';
 import paths from '../paths';
 
-const categories: {[key: string]: string|undefined} = {
+
+
+interface CategoryMap {
+  name: string,
+  videos: Page[]
+}
+
+const categories: {[key: string]: string} = {
   'AA': 'General Spirituality (Meta-Spirituality)',
   'AB': 'Enlightenment',
   'AC': 'Religious Acceptance',
@@ -19,32 +26,44 @@ const categories: {[key: string]: string|undefined} = {
   'AL': 'Conspiracies',
 };
 
-function getCategoryName(category: string) {
-  const cat = categories[category];
-  if (!cat) throw Error('Category Not Found');
-  return cat;
-}
 
-export async function createVideoMap(cb: () => void): Promise<void> {
+
+
+export async function buildVideoMap(cb: () => void): Promise<void> {
   if (!existsSync(paths.dist.library)) mkdirSync(paths.dist.library);
   if (!existsSync(paths.release.library)) mkdirSync(paths.release.library);
 
-  const rawVideos = await getVideos('library/videos', 'published', 'content.category:asc');
+  const videos      = await getVideos('library/videos', 'published', 'content.category:asc');
+  const categoryMap = createCategoryMap(videos);
 
-  const videoMap: {[key: string]: Page[] } = {};
-  rawVideos.forEach(v => {
-    const cat = getCategoryName(v.category!);
-    delete v.category;
-    if(!videoMap[cat]) videoMap[cat] = [];
-    videoMap[cat].push(v);
-  });
-
-  // Sort Videos Descending for each category
-  for (const cat in videoMap) {
-    videoMap[cat].sort((v1, v2) => Date.parse(v1.date!) - Date.parse(v2.date!));
+  // Sort Videos by Ascending Date for each category
+  for (const cat of categoryMap) {
+    cat.videos.sort((v1, v2) => Date.parse(v1.date!) - Date.parse(v2.date!));
   }
 
-  await writeFile(`${paths.dist.library}/videos.json`, JSON.stringify(videoMap, null, 2));
+  await writeFile(`${paths.dist.library}/videos.json`, JSON.stringify(categoryMap, null, 2));
   cb();
 }
+
+
+function createCategoryMap(videos: Video[]) {
+  const categoryMap: CategoryMap[] = [];
+
+  videos.forEach(v => {
+    if (!v.category)                  throw Error('Category Undefined');
+    if (!isValidCategory(v.category)) throw Error('Category Not Found')
+    ;
+    const catName  = categories[v.category];
+    const catIndex = categoryMap.findIndex(cat => cat.name == catName)
+    ;
+    if (!~catIndex) categoryMap.push({ name: catName, videos: [v] });
+    else categoryMap[catIndex].videos.push(v);
+  });
+
+  return categoryMap;
+}
+
+
+const isValidCategory = (name: string) => !!categories[name];
+
 
