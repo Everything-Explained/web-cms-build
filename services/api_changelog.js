f4d9bc2dc6d9eb0b6f8130f3167c8b91b4706243
Jaeiya <exanix@gmail.com>
2021-05-12 15:14:02 -0700

    Update changelog source to use 'published' version

--- services/api_changelog.js
+++ services/api_changelog.js
@@ -5,8 +5,8 @@ const api_storyblok_1 = require("./api_storyblok");
 async function getChangelogs() {
     const stories = await api_storyblok_1.getStories({
         starts_with: 'changelog',
+        version: 'published',
         sort_by: 'created_at:desc',
-        version: 'draft',
     });
     return stories.map(story => {
         const page = api_storyblok_1.mapStoryToPage(story);
