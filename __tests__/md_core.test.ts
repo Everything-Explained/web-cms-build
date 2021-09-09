244fd9ddfffcfb42a682379606a5b8fd6e2c89f1
Jason <exanix@gmail.com>
2021-09-09 15:31:47 -0700

    fix(md_core_test): correct verbiage

--- __tests__/md_core.test.ts
+++ __tests__/md_core.test.ts
@@ -3,28 +3,28 @@ import { useMarkdown } from "../src/services/markdown/md_core";
 const md = useMarkdown();
 
 
-describe('Markdown Expectations', () => {
+describe('Markdown Renderer', () => {
 
-  it('converts Markdown to HTML', () => {
+  it('renders Markdown as HTML', () => {
     const mdString = 'Some text **bolded** and *italisized*.';
     const validOutput = '<p>Some text <strong>bolded</strong> and <em>italisized</em>.</p>\n';
     expect(md.render(mdString)).toBe(validOutput);
   });
 
-  it('converts Markdown links to open in new Tab/Page', () => {
+  it('renders Markdown links to open in new Tab/Page', () => {
     const mdWithLink = '[link](http://t.est)';
     const validOutput = '<p><a href="http://t.est" target="_blank" rel="noopener">link</a></p>\n';
     expect(md.render(mdWithLink)).toBe(validOutput);
   });
 
-  it('converts links to internal links if applicable', () => {
+  it('renders internal links to use $router', () => {
     const mdWithInternalLink = '[link](/test)';
     const validOutput = '<p><a href="/test" onclick="event.preventDefault(); ' +
                         'window.app.$router.push(\'/test\')">link</a></p>\n';
     expect(md.render(mdWithInternalLink)).toBe(validOutput);
   });
 
-  it('converts video syntax to custom embed', () => {
+  it('renders Video Plugin Syntax to YouTube embed code', () => {
     const mdWithVideo = '@[youtube](5JqzCjg4YRU)';
     const validVideoEmbedCode =
       `<p><div class="embed-responsive embed-responsive-16by9">` +
@@ -37,12 +37,12 @@ describe('Markdown Expectations', () => {
     expect(md.render(mdWithVideo)).toBe(validVideoEmbedCode);
   });
 
-  it('throws error with invalid video syntax', () => {
+  it('throws error with invalid Video ID', () => {
     const invalidMDVideo = '@[youtube](58712)';
     expect(() => md.render(invalidMDVideo)).toThrow();
   });
 
-  it('should skip processing if video markdown is incomplete', () => {
+  it('throws error when Video Plugin Syntax is incomplete', () => {
     expect(() => md.render('@[]')).toThrow();
   });
 
