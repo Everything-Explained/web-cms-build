d7ec115fdf5cd3ed421bee3c8bd92c32ffafccbc
jaeiya <exanix@gmail.com>
2022-03-10 13:15:16 -0800

    feat(build_manifest): add `isHashManifest` option
    `isHashManifest` defaults to false, but when set to true, it
    saves the manifest file with the minimum required information
    to test for updates.

--- src/build_manifest.ts
+++ src/build_manifest.ts
@@ -18,7 +18,14 @@ type ManifestEntry = {
 	hash      : string;
 }
 
-type Manifest = ManifestEntry[];
+export type HashManifestEntry = {
+  id    : number|string
+  title : string;
+  hash  : string;
+}
+
+type Manifest     = ManifestEntry[];
+type HashManifest = HashManifestEntry[]
 
 
 export interface BuildOptions {
@@ -35,8 +42,10 @@ export interface BuildOptions {
   api           : StoryblokAPI;
   /** Saves the manifest. Default: **true** */
   canSave?         : boolean;
+  /** Saves the manifest as a hash-only manifest. Default **false** */
+  isHashManifest?  : boolean;
   /** Callback when an entry has been deleted. */
-  onDelete?     : (entry: CMSEntry) => void;
+  onDelete?     : (entry: CMSEntry|HashManifestEntry) => void;
   /** Callback when an entry has been updated. */
   onUpdate?     : (entry: CMSEntry) => void;
   /** Callback when an entry has been Added. */
@@ -45,7 +54,10 @@ export interface BuildOptions {
 
 export interface BuildOptionsInternal extends BuildOptions {
   /** Set as the default manifest file name. */
-  manifestName: string;
+  manifestName   : string;
+  canSave        : boolean;
+  isHashManifest : boolean;
+  isInit         : boolean;
 }
 
 type BuildResult = Promise<[filePath: string, latestEntries: CMSEntry[], hasUpdated: boolean]>;
@@ -55,14 +67,18 @@ type BuildResult = Promise<[filePath: string, latestEntries: CMSEntry[], hasUpda
 
 export async function buildManifest(opts: BuildOptions): BuildResult {
   const { url, api, starts_with, sort_by, version } = opts;
-  opts.buildPath      = pathResolve(opts.buildPath);
-  opts.manifestName ??= pathBasename(opts.buildPath);
-  opts.canSave         ??= true;
+  opts.buildPath        = pathResolve(opts.buildPath);
+  opts.manifestName   ??= pathBasename(opts.buildPath);
+  opts.canSave        ??= true;
+  opts.isHashManifest ??= false;
+
+  const internalOptions = opts as BuildOptionsInternal;
+  internalOptions.isInit = false;
 
   const cmsOptions: CMSOptions = { url, starts_with, sort_by, version, };
 
   const latestEntries  = await useStoryblok(api).getCMSEntries(cmsOptions);
-  const oldEntries     = await getManifestEntries(latestEntries, opts as BuildOptionsInternal);
+  const oldEntries     = await getManifestEntries(latestEntries, internalOptions);
   const detectionFuncs = [
     detectAddedEntries(opts.onAdd),
     detectUpdatedEntries(opts.onUpdate),
@@ -70,14 +86,18 @@ export async function buildManifest(opts: BuildOptions): BuildResult {
   ];
 
   const hasUpdatedEntries = detectionFuncs.map(f => f(oldEntries, latestEntries)).includes(true);
-  let manifest            = oldEntries;
+  let manifest: HashManifest|Manifest = oldEntries;
 
   if (hasUpdatedEntries && opts.canSave) {
-    manifest = latestEntries.map(toManifestEntry);
+    manifest = latestEntries.map(opts.isHashManifest ? toHashManifestEntry : toManifestEntry);
     await saveAsJSON(opts.buildPath, opts.manifestName)(manifest);
   }
 
-  return [pathJoin(opts.buildPath, `/${opts.manifestName}.json`), manifest, hasUpdatedEntries];
+  return [
+    pathJoin(opts.buildPath, `/${opts.manifestName}.json`),
+    latestEntries,
+    internalOptions.isInit || hasUpdatedEntries
+  ];
 }
 
 
@@ -95,11 +115,14 @@ async function getManifestEntries(latestEntries: CMSEntry[], opts: BuildOptionsI
 
 function initManifest(entries: CMSEntry[], opts: BuildOptionsInternal) {
   const { buildPath, manifestName, onAdd, canSave } = opts;
-  tryCreateDir(opts.buildPath);
+  if (canSave == undefined || canSave) {
+    opts.isInit = true;
+    tryCreateDir(opts.buildPath);
+  }
   if (onAdd) {
     entries.forEach(onAdd);
   }
-  const manifest = entries.map(toManifestEntry);
+  const manifest = entries.map(opts.isHashManifest ? toHashManifestEntry : toManifestEntry);
   return (
     canSave == undefined || canSave
       ? saveAsJSON(buildPath, manifestName)(manifest)
@@ -130,8 +153,17 @@ export function toManifestEntry(newEntry: CMSEntry) {
 }
 
 
+export function toHashManifestEntry(newEntry: CMSEntry) {
+  const {id, title, hash} = newEntry;
+  const entry: HashManifestEntry = {
+    id, title, hash,
+  };
+  return entry;
+}
+
+
 function detectAddedEntries(onAddEntries?: (entry: CMSEntry) => void) {
-  return (oldEntries: ManifestEntry[], latestEntries: CMSEntry[]) => {
+  return (oldEntries: Manifest|HashManifest, latestEntries: CMSEntry[]) => {
     let hasAdded = false;
     for (const newEntry of latestEntries) {
       if (!oldEntries.find(hasSameID(newEntry))) {
@@ -146,8 +178,8 @@ function detectAddedEntries(onAddEntries?: (entry: CMSEntry) => void) {
 }
 
 
-function detectDeletedEntries(onDelete?: (oldEntry: CMSEntry) => void) {
-  return (oldEntries: ManifestEntry[], latestEntries: CMSEntry[]) => {
+function detectDeletedEntries(onDelete?: (oldEntry: CMSEntry|HashManifestEntry) => void) {
+  return (oldEntries: Manifest|HashManifest, latestEntries: CMSEntry[]) => {
     let hasDeleted = false;
     for (const oldEntry of oldEntries) {
       if (!latestEntries.find(hasSameID(oldEntry))) {
@@ -163,7 +195,7 @@ function detectDeletedEntries(onDelete?: (oldEntry: CMSEntry) => void) {
 
 
 function detectUpdatedEntries(onUpdate?: (updatedEntry: CMSEntry) => void) {
-  return (oldEntries: ManifestEntry[], latestEntries: CMSEntry[]) => {
+  return (oldEntries: Manifest|HashManifest, latestEntries: CMSEntry[]) => {
     let hasUpdated = false;
     for (const latestEntry of latestEntries) {
       const oldEntry = oldEntries.find(hasSameID(latestEntry));
