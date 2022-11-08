726118a4cf3081bd578655148b8bc72d4965e7c7
jaeiya <exanix@gmail.com>
2022-11-08 14:56:07 -0800

    chore: update compiled files

--- bin/main.d.ts
+++ bin/main.d.ts
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-export declare function build(rootPath: string, destPath: string, done?: () => undefined): void;
+export declare function build(rootPath: string, destPath: string, done?: () => void): void;
