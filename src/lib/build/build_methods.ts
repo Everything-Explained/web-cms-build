3986f135041750b266b222ebb671809cd01e3f54
jaeiya <exanix@gmail.com>
2022-11-29 16:44:28 -0800

    fix(methods): use full path name in `starts_with`
    "starts_with" is a subtext lookup, which means these two lookups will return the same results: "lit", "lit-archive". This is clearly not the desired behavior as both those lookups should be distinct

--- src/lib/build/build_methods.ts
+++ src/lib/build/build_methods.ts
@@ -25,35 +25,35 @@ const partialBuildOptions = {
 
 export const buildPublicBlog = (buildPath: string) => buildLiterature({
   buildPath,
-  starts_with: 'page-data/blog',
+  starts_with: 'page-data/blog/',
   sort_by: 'created_at:desc',
   ...partialBuildOptions,
 });
 
 export const buildRed33mBlog = (buildPath: string) => buildLiterature({
   buildPath,
-  starts_with: 'page-data/blog/red33m',
+  starts_with: 'page-data/blog/red33m/',
   sort_by: 'created_at:desc',
   ...partialBuildOptions,
 });
 
 export const buildChangelog = (buildPath: string) => buildLiterature({
   buildPath,
-  starts_with: 'page-data/changelog',
+  starts_with: 'page-data/changelog/',
   sort_by: 'created_at:desc',
   ...partialBuildOptions,
 });
 
 export const buildPublicLit = (buildPath: string) => buildLiterature({
   buildPath,
-  starts_with: 'page-data/literature/public',
+  starts_with: 'page-data/literature/public/',
   sort_by: 'first_published_at:asc',
   ...partialBuildOptions,
 });
 
 export const buildRed33mLit = (buildPath: string) => buildLiterature({
   buildPath,
-  starts_with: 'page-data/literature/red33m',
+  starts_with: 'page-data/literature/red33m/',
   sort_by: 'first_published_at:asc',
   ...partialBuildOptions,
 });
@@ -61,7 +61,7 @@ export const buildRed33mLit = (buildPath: string) => buildLiterature({
 export const buildPublicVideos = (buildPath: string) => buildVideos({
   buildPath,
   fileName: 'public',
-  starts_with: 'page-data/videos/public',
+  starts_with: 'page-data/videos/public/',
   sort_by: 'content.timestamp:asc',
   catList_starts_with: 'utils/category-list',
   ...partialBuildOptions
@@ -70,7 +70,7 @@ export const buildPublicVideos = (buildPath: string) => buildVideos({
 export const buildRed33mVideos = (buildPath: string) => buildVideos({
   buildPath,
   fileName: 'red33m',
-  starts_with: 'page-data/videos/red33m',
+  starts_with: 'page-data/videos/red33m/',
   sort_by: 'content.timestamp:asc',
   ...partialBuildOptions
 });
