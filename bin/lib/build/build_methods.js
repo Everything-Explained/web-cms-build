c45db1b5cb02b6d71eca2f20ed1a1080fd8c235c
jaeiya <exanix@gmail.com>
2022-12-01 10:43:59 -0800

    chore: update compiled files

--- bin/lib/build/build_methods.js
+++ bin/lib/build/build_methods.js
@@ -14,14 +14,14 @@ const partialBuildOptions = {
 const buildPublicBlog = (buildPath) => (0, build_literature_1.buildLiterature)({
     buildPath,
     starts_with: 'page-data/blog/public/',
-    sort_by: 'first_published_at:desc',
+    sort_by: 'first_published_at:asc',
     ...partialBuildOptions,
 });
 exports.buildPublicBlog = buildPublicBlog;
 const buildRed33mBlog = (buildPath) => (0, build_literature_1.buildLiterature)({
     buildPath,
     starts_with: 'page-data/blog/red33m/',
-    sort_by: 'first_published_at:desc',
+    sort_by: 'first_published_at:asc',
     ...partialBuildOptions,
 });
 exports.buildRed33mBlog = buildRed33mBlog;
